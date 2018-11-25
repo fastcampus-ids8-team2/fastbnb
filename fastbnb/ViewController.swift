64b3106f54eba9e32579f3f40cb7e00999dd1cae
WooJun Kim <kwj2870@gmail.com>
2018-11-25 21:54:54 +0900

    Initial Commit

---
+++ fastbnb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fastbnb
+//
+//  Created by WooJun Kim on 25/11/2018.
+//  Copyright © 2018 fastcampus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
