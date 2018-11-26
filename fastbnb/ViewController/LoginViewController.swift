6227f24bf9ce43e07c37009eba0a6fe00625c513
WooJun Kim <kwj2870@gmail.com>
2018-11-26 17:32:15 +0900

    Update LoginViewController

--- fastbnb/ViewController/LoginViewController.swift
+++ fastbnb/ViewController/LoginViewController.swift
@@ -8,27 +8,63 @@
 
 import UIKit
 import FacebookLogin
+import AloeStackView
+import FBSDKCoreKit
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var headerView: UIView!
-    @IBOutlet weak var contentsView: UIView!
-    @IBOutlet weak var footerView: UIView!
+    @IBOutlet private weak var headerView: UIView!
+    @IBOutlet private weak var contentsView: UIView!
+    @IBOutlet private weak var footerView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let facebookLoginButton = LoginButton(readPermissions: [ .publicProfile ])
-        let googleLoginButton = UIButton()
+        let aloeStackView = AloeStackView()
+        contentsView.addSubview(aloeStackView)
         
-        facebookLoginButton.translatesAutoresizingMaskIntoConstraints = false
-        googleLoginButton.translatesAutoresizingMaskIntoConstraints = false
+        aloeStackView.translatesAutoresizingMaskIntoConstraints = false
+        aloeStackView.topAnchor.constraint(equalTo: contentsView.topAnchor, constant: 0).isActive = true
+        aloeStackView.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor, constant: 0).isActive = true
+        aloeStackView.bottomAnchor.constraint(equalTo: contentsView.bottomAnchor, constant: 0).isActive = true
+        aloeStackView.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor, constant: 0).isActive = true
+        aloeStackView.hidesSeparatorsByDefault = true
         
-        contentsView.addSubview(facebookLoginButton)
-    }
-
-    func addContraints() {
-        
+        let facebookLoginButton = LoginButton(readPermissions: [ .publicProfile, .email ])
+        facebookLoginButton.delegate = self
+        aloeStackView.addRow(facebookLoginButton)
     }
 }
 
+extension LoginViewController: LoginButtonDelegate {
+    func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
+        switch result {
+        case .success(_, _, let token):
+            print(token.authenticationToken)
+            
+            let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"id, email, name"])
+            graphRequest.start(completionHandler: { (connection, result, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+                let fbData = result as! [String:AnyObject]
+                let userName = fbData["name"]! as? NSString
+                let userEmail = fbData["email"]! as? NSString
+                
+                print("userName:",userName!)
+                print("userEmail",userEmail!)
+            })
+        case .failed(let error):
+            print(error.localizedDescription)
+        case .cancelled:
+            print("cancelled")
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: LoginButton) {
+        print("loginButtonDidLogOut")
+    }
+    
+    
+}
