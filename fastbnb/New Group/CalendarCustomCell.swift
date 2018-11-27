bfe7e7b077adb3ac52178401323ebad3b49b982e
Isaac Kim <myong328@gmail.com>
2018-11-27 13:18:48 +0900

    calendar view started with explore

---
+++ fastbnb/New Group/CalendarCustomCell.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarCustomCell.swift
+//  fastbnb
+//
+//  Created by Isaac Kim on 27/11/2018.
+//  Copyright © 2018 fastcampus. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarCustomCell: JTAppleCell {
+    @IBOutlet private weak var dataLabel: UILabel!
+
+   
+
+}
