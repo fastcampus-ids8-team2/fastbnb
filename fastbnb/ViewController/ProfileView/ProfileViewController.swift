5f8709bed2c4c5c4cb48a6f71de19ed993c6472a
wonsik <>
2018-11-29 22:17:14 +0900

    ProfileItem.ProfileList추가

--- fastbnb/ViewController/ProfileView/ProfileViewController.swift
+++ fastbnb/ViewController/ProfileView/ProfileViewController.swift
@@ -8,17 +8,44 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController,UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
+    private let profileList = ProfileList()
+    private var items: [ProfileItem] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        items = profileList.items
+        tableView.rowHeight = 65
       
     }
-    
-
-
 
 }
+
+extension ProfileViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ProfileCell.identifier, for: indexPath) as! ProfileCell
+        
+        cell.setupCell(title: items[indexPath.row].title, imageName: items[indexPath.row].imageName)
+        return cell
+        
+        
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
