40a5d76d1adf3d317646daa2e67500b33c4fd655
wonsik <>
2018-11-30 03:59:47 +0900

    profilebutton

---
+++ fastbnb/ViewController/ProfileView/ProfileButton.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileButton.swift
+//  fastbnb
+//
+//  Created by wonsik on 30/11/2018.
+//  Copyright © 2018 fastcampus. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+
+class Button: UIButton {
+    @IBInspectable var cornerRadius: CGFloat {
+        get { return layer.cornerRadius}
+        set { layer.cornerRadius = newValue}
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {return layer.borderWidth }
+        set { layer.borderWidth = newValue} 
+        
+        
+    }
+    
+
+    @IBInspectable var borderColor: UIColor {
+        get { return UIColor(cgColor: layer.borderColor ?? UIColor.blue.cgColor)}
+        set { layer.borderColor = newValue.cgColor}
+    }
+}
