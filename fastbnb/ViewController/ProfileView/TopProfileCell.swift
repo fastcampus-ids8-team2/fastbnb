39eecf5aa476e4d636f7d282c7be868a69a276a9
wonsik <>
2018-12-10 06:23:19 +0900

    test

---
+++ fastbnb/ViewController/ProfileView/TopProfileCell.swift
@@ -0,0 +1,19 @@
+//
+//  TopProfileCell.swift
+//  fastbnb
+//
+//  Created by wonsik on 10/12/2018.
+//  Copyright © 2018 fastcampus. All rights reserved.
+//
+
+//import UIKit
+//
+//class TopProfileCell: UITableViewCell {
+//
+//
+////
+//@IBOutlet var toptitle: UILabel!
+//@IBOutlet var topexplain: UILabel!
+//@IBOutlet var topicon: UIImageView!
+
+//}
