f48a4a58403061811b37c6e327271b2d5cb385fc
wonsik <>
2018-12-14 14:07:04 +0900

    test

--- fastbnb/ViewController/ProfileView/ProfileViewController.swift
+++ fastbnb/ViewController/ProfileView/ProfileViewController.swift
@@ -19,6 +19,7 @@ class ProfileViewController: UIViewController{
         bottomTableView.rowHeight = 70
     }
     let data = ProfileData()
+    let topData = TopProfileData()
     
     }
 
@@ -26,6 +27,7 @@ class ProfileViewController: UIViewController{
 extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.data.list.count
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -36,18 +38,18 @@ extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
         cell.icon?.image = UIImage(named: list.icon)
         return cell
     }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         NSLog("선택된행은 \(indexPath.row) 번째 행입니다")
         
-        switch indexPath.row {            
+        switch indexPath.row {
         case 0: self.performSegue(withIdentifier: "a", sender: nil)
         case 1: self.performSegue(withIdentifier: "b", sender: nil)
         case 2: self.performSegue(withIdentifier: "c", sender: nil)
         case 3: self.performSegue(withIdentifier: "d", sender: nil)
         case 4: self.performSegue(withIdentifier: "e", sender: nil)
         case 5: self.performSegue(withIdentifier: "f", sender: nil)
-        
         default:
             return
         }
