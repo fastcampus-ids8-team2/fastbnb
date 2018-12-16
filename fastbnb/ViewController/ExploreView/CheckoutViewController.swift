f0b46c08222655ef9bda929be3d3c0d9061b216c
Isaac Kim <myong328@gmail.com>
2018-12-16 15:19:50 +0900

    layout for the checkoutViewController has been created

--- fastbnb/ViewController/ExploreView/CheckoutViewController.swift
+++ fastbnb/ViewController/ExploreView/CheckoutViewController.swift
@@ -10,16 +10,75 @@ import UIKit
 
 class CheckoutViewController: UIViewController {
 
+    @IBOutlet weak var personalTripButton: UIButton!
+    @IBOutlet weak var businessTripButton: UIButton!
+    
     @IBOutlet weak var requestToBook: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    
- 
-         requestToBook.backgroundColor = #colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1)
-         requestToBook.layer.cornerRadius = 10
+        setupInitialButton()
+    }
+    private func setupInitialButton() {
+        // initial button Business and personal trip
+        businessTripButton.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        businessTripButton.layer.borderWidth = 1
+        businessTripButton.layer.cornerRadius = 5
+        businessTripButton.layer.backgroundColor = #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1)
+        businessTripButton.backgroundColor = #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1)
+        businessTripButton.setTitle("     Business trip     ", for: .normal)
+        businessTripButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
         
+        personalTripButton.layer.borderColor = #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1)
+        personalTripButton.layer.borderWidth = 1
+        personalTripButton.layer.cornerRadius = 5
+        personalTripButton.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        personalTripButton.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        personalTripButton.setTitle("     Personal trip     ", for: .normal)
+        personalTripButton.setTitleColor(#colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1), for: .normal)
+        
+        // request to book Button
+        requestToBook.backgroundColor = #colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1)
+        requestToBook.layer.cornerRadius = 10
+    }
+    @IBAction func didButtonTapPersonalTrip(_ sender: Any) {
+        personalTripButton.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        personalTripButton.layer.borderWidth = 1
+        personalTripButton.layer.cornerRadius = 5
+        personalTripButton.layer.backgroundColor = #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1)
+        personalTripButton.backgroundColor = #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1)
+        personalTripButton.setTitle("     Personal trip     ", for: .normal)
+        personalTripButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        
+        businessTripButton.layer.borderColor = #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1)
+        businessTripButton.layer.borderWidth = 1
+        businessTripButton.layer.cornerRadius = 5
+        businessTripButton.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        businessTripButton.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        businessTripButton.setTitle("     Business trip     ", for: .normal)
+        businessTripButton.setTitleColor(#colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1), for: .normal)
+    }
+    
+    @IBAction func didButtonTapBusinessTrip(_ sender: Any) {
+        businessTripButton.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        businessTripButton.layer.borderWidth = 1
+        businessTripButton.layer.cornerRadius = 5
+        businessTripButton.layer.backgroundColor = #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1)
+        businessTripButton.backgroundColor = #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1)
+        businessTripButton.setTitle("     Business trip     ", for: .normal)
+        businessTripButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        
+        personalTripButton.layer.borderColor = #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1)
+        personalTripButton.layer.borderWidth = 1
+        personalTripButton.layer.cornerRadius = 5
+        personalTripButton.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        personalTripButton.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        personalTripButton.setTitle("     Personal trip     ", for: .normal)
+        personalTripButton.setTitleColor(#colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1), for: .normal)
         
     }
-
+    
+    
 }
+
+
